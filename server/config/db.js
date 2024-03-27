dcd5c2f4b410292a885ae3b57375dda9678c9eb5
Akshat srivastav <122502920+akshatsri47@users.noreply.github.com>
2024-03-28 02:08:57 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async()=>{
     try{
         mongoose.set('strict',false);
-        const conn = await mongoose.connect('mongodb+srv://akshatsrivastav38:kMpCZtGFxGYbHZoE@cluster0.n4csv8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        const conn = await mongoose.connect('url');
         console.log(`database connected : ${conn.connection.host}`);
     }
     catch(error){
